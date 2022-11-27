92e0fc71b85671f921a85448d9ff3c619a223698
Ádám Plesoczki <43034243+plesoczkiadam@users.noreply.github.com>
2022-11-27 18:50:03 +0100

    Add files via upload

---
+++ minumum/Plesoczki_A_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plesoczki_A_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, db;
+            Console.WriteLine("Hány nap hómérsékleti adatát kérjem be?");
+            Console.Write("n= ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] ho = new int[n];
+            db = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}.nap: ",i+1);
+                ho[i] = Convert.ToInt32(Console.ReadLine());
+                if (ho[i] >= 15)
+                    db++;
+
+            }
+            Console.WriteLine("{0} napon volt minimum 15 fok vagy melegebb");
+            Console.ReadLine();
+        }
+    }
+}
